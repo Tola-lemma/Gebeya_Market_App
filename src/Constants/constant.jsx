fab458df5407bef14202ddaf5e9ce64da914f13c
Tola-lemma <tolalemma@gmail.com>
2024-11-24 10:36:47 +0300

    feat:Constants is initialized for latter use

---
+++ src/Constants/constant.jsx
@@ -0,0 +1,17 @@
+export const NAME = "GEBEYA MARKET APP"
+export const colors = {
+      background: {
+        primary:  "#F2F2F2",
+        secondary:  "#DEA449",
+        tertiary: "#61CE70",
+        outline: "#BBBDBF",
+        gray:'#717D8A',
+        lightGray:'#5D6A78',
+        warning:  "#0071BC",
+        error: "#EE5D5D",
+        green:  "#8EDF87",
+        black:'#000000',
+        darkLayerAppBar: "#EAEAEA",
+        pureWhite: "#ffffff",
+      },
+    };
