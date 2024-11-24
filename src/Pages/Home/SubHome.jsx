9f2c83ec56ba1e56a2b906325dd8ff8c2f1edf49
Tola-lemma <tolalemma@gmail.com>
2024-11-24 22:11:14 +0300

    feat:home page background is added

--- src/Pages/Home/SubHome.jsx
+++ src/Pages/Home/SubHome.jsx
@@ -1,8 +1,10 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import { colors } from '../../Constants/constant'
 
 const SubHome  = () =>{
   return (
-    <div>Welcome To SubHome Page</div>
+    <Box sx={{height:"769px",backgroundColor:colors.background.primary,width:"1512",fontFamily:"alfa slab one"}}>Welcome To SubHome Page</Box>
   )
 }
 
