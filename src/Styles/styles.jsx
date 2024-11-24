bddd36b50f29bc4bc64ec410077d2e7d619e4e73
Tola-lemma <tolalemma@gmail.com>
2024-11-24 15:10:38 +0300

    Style:Style Element is Added using Styled mui

---
+++ src/Styles/styles.jsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+import {  Toolbar } from "@mui/material";
+// import { colors } from "../Constants/constant";
+export const StyledImage = styled("img")({
+  alt: "Styled image",
+});
+export const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+// export const SearchField = styled("div")(({theme})=>({
+// backgroundColor:"red",
+// padding:"0 10px",
+// borderRadius:"50px"
+// }))
