1a1057fac09c6bf878aa085da9bc446f160b16bd
Tola-lemma <tolalemma@gmail.com>
2024-11-24 15:06:15 +0300

    Style:Some color is modified

--- src/Constants/constant.jsx
+++ src/Constants/constant.jsx
@@ -1,14 +1,14 @@
 export const NAME = "GEBEYA MARKET APP"
 export const colors = {
       background: {
-        primary:  "#F2F2F2",
-        secondary:  "#DEA449",
+        primary:  "#882BEC",
+        secondary:  "#FEA301",
         tertiary: "#61CE70",
         outline: "#BBBDBF",
         gray:'#717D8A',
         lightGray:'#5D6A78',
         warning:  "#0071BC",
-        error: "#EE5D5D",
+        error: "#FF0000",
         green:  "#8EDF87",
         black:'#000000',
         darkLayerAppBar: "#EAEAEA",
