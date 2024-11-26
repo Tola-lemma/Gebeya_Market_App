4a11c898f05f5f00e7b7faa008ee0d5863fb28cf
Tola-lemma <tolalemma@gmail.com>
2024-11-26 13:09:50 +0300

    feat:rating and type of collection added to the bottom of the cards

--- src/Pages/Collection/Collection.jsx
+++ src/Pages/Collection/Collection.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Rating, Typography } from '@mui/material'
 import React from 'react'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -44,132 +44,225 @@ const Collection  = () =>{
         </Box>
       </Box>
       <Box
-  sx={{
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "stretch", // Ensures cards stretch equally
-    padding: "6px",
-    margin:"2.5rem",
-    marginTop:"16px"
-  }}
->
-  {[frenchKissBag, KidsShoe, WomenC].map((imageSrc, index) => (
-    <Card
-      key={index}
-      sx={{
-        width: 450,
-        height: 400, 
-        borderRadius: "16px",
-        border: "1px solid #C1C1C1",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between", 
-        alignItems: "center",
-        paddingBottom: "16px",
-        position:"relative"
-      }}
-    >
-<Box
-    sx={{
-      position: "absolute",
-      top: 20,
-      left: 20, 
-      display: "flex",
-      gap: 37, 
-    }}
-  >
-    <Checkbox
-      sx={{
-        backgroundColor: "#FFFFFF",
-        borderRadius: "50%", // Optional: Makes the checkbox background circular
-      }}
-      {...label}
-      icon={<FavoriteBorder />}
-      checkedIcon={<Favorite />}
-    />
-    <Box
-      sx={{
-        backgroundColor: "#FFFFFF",
-        fontFamily:"Poppins",
-        borderRadius: "17px",
-        alignContent:"center",
-        width:"70px",
-        height: "40px",
-        textAlign:"center"
-      }}
-    > In Stock
-    </Box>
-  </Box>
-   {/* Image Section */}
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: 258, // Fixed height for image area
-          width: "420px",
-          backgroundColor: "#F2F2F2",
-          borderRadius: "12px",
-          marginTop: "10px",
-        }}
-      >
-        <img
-          style={{
-            maxWidth: "100%",
-            maxHeight: "100%",
-            objectFit: "contain",
-            borderRadius: "12px",
-          }}
-          src={imageSrc}
-          alt={`Card ${index + 1}`}
-        />
-      </Box>
-
-      {/* Card Content */}
-      <CardContent
-        sx={{
-          width: "100%",
-          textAlign: "left",
-          marginLeft: "1.5rem",
-          flexGrow: 1, // Makes content take up available space
-        }}
-      >
-        <Typography gutterBottom variant="h5" component="div" sx={{fontFamily:"Poppins",fontWeight:"bold"}}>
-          {index === 0 ? "French Kiss Bag" : index === 1 ? "Burberry Shine" : "Alvero Gown"}
-        </Typography>
-        <Typography variant="body2" sx={{ color: "text.secondary",fontFamily:"Poppins" }}>
-          {index === 0 ? "ALDO" : index === 1 ? "FENDI" : "Divine"}
-        </Typography>
-      </CardContent>
-
-      {/* Card Actions */}
-      <CardActions
         sx={{
           display: "flex",
           justifyContent: "space-between",
-          width: "100%",
-          padding: "0 16px",
+          alignItems: "stretch", // Ensures cards stretch equally
+          padding: "6px",
+          margin: "2.5rem",
+          marginTop: "16px",
         }}
       >
-        <Button size="24px" sx={{ textTransform: "none",color:"black",fontWeight:"bold",fontFamily:"Poppins" }}>
-          ${index === 0 ? 500 : index === 1 ? 150 : 300}
-        </Button>
-        <Button
-          size="large"
-          sx={{
-            backgroundColor: "#FEA301",
-            color: "white",
-            textTransform: "none",
-            marginRight:"20px"
-          }}
-        >
-          Add to cart
-        </Button>
-      </CardActions>
-    </Card>
-  ))}
-</Box>
+        {[frenchKissBag, KidsShoe, WomenC].map((imageSrc, index) => (
+          <Card
+            key={index}
+            sx={{
+              width: 450,
+              height: 450,
+              borderRadius: "16px",
+              border: "1px solid #C1C1C1",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingBottom: "16px",
+              position: "relative",
+            }}
+          >
+            <Box
+              sx={{
+                position: "absolute",
+                top: 20,
+                left: 20,
+                display: "flex",
+                gap: 37,
+              }}
+            >
+              <Checkbox
+                sx={{
+                  backgroundColor: "#FFFFFF",
+                  borderRadius: "50%", // Optional: Makes the checkbox background circular
+                }}
+                {...label}
+                icon={<FavoriteBorder />}
+                checkedIcon={<Favorite />}
+              />
+              <Box
+                sx={{
+                  backgroundColor: "#FFFFFF",
+                  fontFamily: "Poppins",
+                  borderRadius: "17px",
+                  alignContent: "center",
+                  width: "70px",
+                  height: "40px",
+                  textAlign: "center",
+                }}
+              >
+                {" "}
+                In Stock
+              </Box>
+            </Box>
+            {/* Image Section */}
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: 258, // Fixed height for image area
+                width: "420px",
+                backgroundColor: "#F2F2F2",
+                borderRadius: "12px",
+                marginTop: "10px",
+              }}
+            >
+              <img
+                style={{
+                  maxWidth: "100%",
+                  maxHeight: "100%",
+                  objectFit: "contain",
+                  borderRadius: "12px",
+                }}
+                src={imageSrc}
+                alt={`Card ${index + 1}`}
+              />
+            </Box>
+            <Box
+              sx={{
+                position: "absolute",
+                top: 270,
+                left: 20,
+                display: "flex",
+                gap: 30,
+              }}
+            >
+              <Box
+                sx={{
+                  backgroundColor: "#FFFFFF",
+                  fontFamily: "Poppins",
+                  borderRadius: "20px",
+                  alignContent: "center",
+                  width: "70px",
+                  height: "40px",
+                  textAlign: "center",
+                  border:"0.5px solid #C1C1C1",
+                  color:"#808080"
+                }}
+              >
+                {index === 0 ? "women" : index === 1 ? "kids" : "women"}
+              </Box>
+              <Box
+                sx={{
+                  backgroundColor: "#FFFFFF",
+                  fontFamily: "Poppins",
+                  borderRadius: "15px",
+                  alignContent: "center",
+                  width: "100px",
+                  height: "40px",
+                  fontSize:"16px",
+                  textAlign: "center",
+                }}
+              >
+                {index === 0 ? (
+                  <>
+                    <Rating
+                      name="size-small"
+                      defaultValue={1}
+                      size="small"
+                      max={1}
+                    />
+                    2k+ rating
+                  </>
+                ) : index === 1 ? (
+                  <>
+                    <Rating
+                      name="size-small"
+                      defaultValue={1}
+                      size="small"
+                      max={1}
+                    />
+                    1k+ rating
+                  </>
+                ) : (
+                  <>
+                    <Rating
+                      name="size-small"
+                      defaultValue={1}
+                      size="small"
+                      max={1}
+                    />
+                    5k+ rating
+                  </>
+                )}
+              </Box>
+            </Box>
+            {/* Card Content */}
+            <CardContent
+              sx={{
+                width: "100%",
+                textAlign: "left",
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+                marginLeft: "1.7rem",
+                marginTop: "1rem",
+                flexGrow: 1, // Makes content take up available space
+              }}
+            >
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                sx={{ fontFamily: "Poppins", fontWeight: "bold" }}
+              >
+                {index === 0
+                  ? "French Kiss Bag"
+                  : index === 1
+                  ? "Burberry Shine"
+                  : "Alvero Gown"}
+              </Typography>
+              <Typography
+                variant="body2"
+                sx={{ color: "text.secondary", fontFamily: "Poppins" }}
+              >
+                {index === 0 ? "ALDO" : index === 1 ? "FENDI" : "Divine"}
+              </Typography>
+            </CardContent>
 
+            {/* Card Actions */}
+            <CardActions
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "100%",
+                padding: "0 16px",
+              }}
+            >
+              <Button
+                size="24px"
+                sx={{
+                  textTransform: "none",
+                  color: "black",
+                  fontWeight: "bold",
+                  fontFamily: "Poppins",
+                }}
+              >
+                ${index === 0 ? 500 : index === 1 ? 150 : 300}
+              </Button>
+              <Button
+                size="large"
+                sx={{
+                  backgroundColor: "#FEA301",
+                  color: "white",
+                  textTransform: "none",
+                  marginRight: "20px",
+                }}
+              >
+                Add to cart
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
+      </Box>
     </Box>
   );
 }
