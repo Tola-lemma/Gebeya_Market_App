ea3c2432ca0df9691caaa421154de08000b0c9ef
Tola-lemma <tolalemma@gmail.com>
2024-11-27 10:47:25 +0300

    feat:cart context is provided in root directory

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom";
 import AllRoute from './routes';
+import {CartProvider} from './Pages/CartsPages/CartContext'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <CartProvider>
       <Router>
         <AllRoute />
       </Router>
+    </CartProvider>
   </React.StrictMode>
 );
 // reportWebVitals();
